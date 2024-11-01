d854a83bc702be49b07e182884c70ad0605b1f4a
Shagu-blip <datkavarshagula@gmail.com>
2024-11-01 21:38:05 +0400

    second commit

---
+++ Day2/homework.py
@@ -0,0 +1,11 @@
+name="chemi saxelia daviti."
+surname="chemi gvari aris shagulashvili."
+age="me var 12 wlis."
+height="chemi simagle aris 174.4 santimetri."
+country="me vcxovrob qveyanashi saxelad saqartvelo."
+city="me aseve vcxovrob qalaq tbilishi."
+family="chemi ojaxi shedgeba ori adamianisgan:mamachemi;dedachemi;chemi dzma da me."
+favourite_food="chemi sayvareli sawmeli aris xinkali."
+goodbye="did madlobas gixdit yuradgebistvis. momaval shexvedramde!"
+
+print(name+" "+surname+" "+age+" "+height+" "+country+" "+city+" "+family+" "+favourite_food+" "+goodbye)
---
+++ Day2/lesson2.py
@@ -0,0 +1,29 @@
+#name="davit" #ეს არის str (string) ტიპის ცვლადი
+#surname="shagulashvili"
+# name არის ცვლადი
+# = არის ცვლადისთვის მნიშვნელობის მიმნიჭებნელი სიმბოლო
+# "davit" არის ცვლადისთვის მნიშვნელობა
+
+#print(name)
+#პრინტ ფუნქციას გადაეცემა ეკრანზე გამოსატანი ობიექტი
+
+#age=22 #ეს არის int (integer) მთელი რიცხვი
+#height=190.4 #ეს არის float ტიპის ცვლადი (ათწილადი)
+#booLean (booL) ტიპის ცვლადი
+
+
+#knows_programming=True #True ან False
+#is_ugly=False #snakecase(უბრალოდ წერის სტილი სტანდარტულად)
+
+#isUgly=False #ჯავასკრიპტული cameLcase
+
+#print(name+ " " +surname)
+
+#სტრინგი არის ბრჭყალებში მოქცეული სიმბოლოები
+#print(type(age))
+#print(type(name))
+#print(type(surname))
+#print(type(height))
+#print(type(knows_programming))
+
+#print(name + " "+ str(age))
